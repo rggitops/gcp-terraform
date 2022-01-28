ca2186094e4db9e0bd13238ffb1c9dd014bd3103
Rohan Gupta <75747077+rggitops@users.noreply.github.com>
2022-01-29 01:32:13 +0530

    adding VM instance along with the VPC

--- main.tf
+++ main.tf
@@ -25,6 +25,23 @@ provider "google" {
   zone    = "asia-south1-a"
 }
 
-# resource "google_compute_network" "vpc_network" {
-#   name = "terraform-network"
-# }
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
