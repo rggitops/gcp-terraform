19738ff88a0029c64d5ed5e8f8a74226c5e427dd
Rohan Gupta <75747077+rggitops@users.noreply.github.com>
2022-01-29 00:57:34 +0530

    main files

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "scaleworx-sandbox"
+
+    workspaces {
+      name = "google-cloud-demo"
+    }
+  }
+}
+
+provider "google" {
+  #credentials = file("<premium-highway-319908-c7c8a9bf4eb0.json")
+
+  project = "premium-highway-319908"
+  region  = "asia-south1"
+  zone    = "asia-south1-a"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
